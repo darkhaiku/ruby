2ac81ba5662e3c07e40bebcd5fcf1d6404bf9229
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-11-17 00:39:45 +0330

    12 Looping in views

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,10 @@
+class ReviewsController < ApplicationController
+
+    def index
+
+        @reviews = ['Lives of the Stoics', 'How to Talk to Anyone', 'Rich Dads Cashflow Quadrant']
+
+    end
+
+
+end
